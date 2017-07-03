b03def3e220345a3278ea57f5000ebcdbb8f67f0
warlockz <yogerocks@gmail.com>
2017-07-03 19:49:31 +0530

    inherited video parent class

--- oop/media.py
+++ oop/media.py
@@ -1,8 +1,15 @@
 import webbrowser
 
-class Movie:
-	def __init__(self,title,storyline,poster_image_url,trailer_youtube_url):
+class VideoParent():
+	def __init__(self,title):
 		self.title = title
+		#self.duration = duration
+
+class Movie(VideoParent):
+	"""docstring for Movie Class"""
+	def __init__(self,title,storyline,poster_image_url,trailer_youtube_url):
+		#self.title = title
+		VideoParent.__init__(self,title)
 		self.storyline = storyline
 		#storyline = storyline  (It will not create as an class variable just random local variable to fn)
 		self.poster_image_url = poster_image_url
@@ -10,3 +17,9 @@ class Movie:
 
 	def show_trailer():
 		webbrowser.open(self.trailer_youtube_url)
+
+class TVShow(VideoParent):
+	"""docstring for TVshow"""
+	def __init__(self,title):
+		VideoParent.__init__(self,title)
+		
